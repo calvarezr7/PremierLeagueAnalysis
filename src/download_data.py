c7149b4157b6d4f1e56dd89787d6c76c8cece588
calvarezr7 <alvarezrcamilo@gmail.com>
2025-02-11 15:11:10 -0500

    Creación del  proyecto

---
+++ src/download_data.py
@@ -0,0 +1,12 @@
+import kaggle
+import os
+
+#Setting the download path
+DATA_PATH = r"C:\Users\camilo.alvarez\Documents\TrainingDE\MiProyecto\data\raw"
+os.makedirs(DATA_PATH, exist_ok= True)
+
+# Descargar el dataset en el directorio actual
+dataset = 'panaaaaa/english-premier-league-and-championship-full-dataset'
+kaggle.api.dataset_download_files(dataset, path= DATA_PATH, unzip=True)
+
+print ("✅ Datos descargados en:", DATA_PATH)
